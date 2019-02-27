3480d14edb44b36768404ba97c2ab936f459812b
d1nger <n.filipchuk@gmail.com>
2019-02-27 03:54:04 -0800

    read local gstreamer stream

--- server-tcp.js
+++ server-tcp.js
@@ -18,11 +18,18 @@ app.use(express.static(__dirname + '/vendor/dist'));
 const server  = http.createServer(app);
 
 const feed    = new RemoteTCPFeedRelay(server, {
-  feed_ip   : "172.19.20.165",
-  feed_port : 5001,
+  feed_ip   : "127.0.0.1",
+  feed_port : 7001,
 });
 
-
 server.listen(8080);
+/*
+Server=> 
+  gst-launch-1.0 -v filesrc location=./samples/out.h264 ! h264parse ! video/x-h264,stream-format=byte-stream ! tcpserversink port=7001 host=0.0.0.0 recover-policy=keyframe sync-method=latest-keyframe sync=true
 
+Cleint=>
+  gst-launch-1.0 -v tcpclientsrc host=127.0.0.1 port=7001 ! typefind ! h264parse ! avdec_h264 ! videoconvert ! autovideosink
 
+1Pipeline=>
+  gst-launch-1.0 filesrc location=./samples/out.h264 ! video/x-h264 ! h264parse ! avdec_h264 ! videoconvert ! videorate ! video/x-raw,framerate=30/1 ! autovideosink
+*/
